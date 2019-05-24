62ceea0f5b4247c96df5057a138217fab863efb9
Mikey2266 <601876177@qq.com>
2019-05-24 16:26:50 +0800

    20190524

---
+++ cpp/stupid.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+// #include<vector>
+
+using namespace std;
+
+int main() {
+    string a = "abcd";
+    char b = 's';
+    // string c;
+    // c = a + b;
+    // cout << c;
+    cout << a+b;
+    return 0;
+}
