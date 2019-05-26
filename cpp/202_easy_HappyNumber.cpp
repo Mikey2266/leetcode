e3b4f89e49217e39153f030a82e0709cf31fa8e0
Mikey2266 <601876177@qq.com>
2019-05-26 14:17:35 +0800

    0526_91st

---
+++ cpp/202_easy_HappyNumber.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+#include<vector>
+#include<windows.h>
+#include<algorithm>
+#include<unordered_set>
+
+using namespace std;
+
+class Solution {
+public:
+    bool isHappy(int n) {
+        unordered_set<int> s;
+        while (n != 1) {
+            int sum = 0;
+            while (n) {
+                sum += (n % 10) * (n % 10);
+                n /= 10;
+            }
+            n = sum;
+            if (s.count(n)) break;
+            s.insert(n);
+        }
+        return n == 1;
+    }
+};
+
+int main() {
+    int x = 7;
+    // long start = GetTickCount();
+    cout << Solution().isHappy(x);
+    // long end = GetTickCount();
+    cout << endl;
+    // cout << end - start << endl;
+    return 0;
+}
